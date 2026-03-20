01f65a5c80305c36592146f0afed81a8af6a2f5a
TomaszWiniarski99 <s33704@pjwstk.edu.pl>
2026-03-20 16:51:02 +0100

    Expand implementation for Renting.

--- ProjektC1/Renting.cs
+++ ProjektC1/Renting.cs
@@ -5,15 +5,32 @@ public class Renting
     public Person Renter { get; set; }
     public Item RentedItem { get; set; }
     public int Id { get; set; }
-    public int DaysRented { get; set; } = 0;
+    public DateTime RentDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public double Penalty { get; set; }
+    public bool IsReturned { get; set; }
 
     public static int IdCount { get; set; }
 
-    public Renting(Person renter, Item rentedItem)
+    public Renting(Person renter, Item rentedItem, int days)
     {
-        this.Renter = Renter;
-        this.RentedItem = RentedItem;
-        Id = IdCount;
-        IdCount++;
+        Renter = renter;
+        RentedItem = rentedItem;
+        Id = IdCount++;
+        RentDate = DateTime.Now;
+        DueDate = RentDate.AddDays(days);
+        IsReturned = false;
+    }
+    
+    public void ReturnItem()
+    {
+        ReturnDate = DateTime.Now;
+        IsReturned = true;
+        if (ReturnDate > DueDate)
+        {
+            int lateDays = (ReturnDate.Value - DueDate).Days;
+            Penalty = lateDays * 10;
+        }
     }
 }
